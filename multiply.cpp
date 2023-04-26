5c587ff81b262f5802ca0378d0ec946a44542210
M. Hassaan <hassaanbasil@gmail.com>
2023-04-26 11:19:36 +0500

    multiply.cpp comitted in branch hassaan/multiply

---
+++ multiply.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int num1, num2;
+    num1 = 10;
+    num2 = 20;
+    int product = num1*num2;
+    cout << "The product is: " << product;
+
+    cout << endl;
+    return 0;
+}
