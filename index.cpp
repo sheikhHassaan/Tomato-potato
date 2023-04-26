19b999cb3bc1c5cdba3eb4e81a3975810f891279
M. Hassaan <hassaanbasil@gmail.com>
2023-04-26 10:41:23 +0500

    index.cpp Sum programm commit 1

---
+++ index.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int num1, num2, sum;
+    string prompt1 = "Enter the first number: ";
+    string prompt2 = "Enter the second number: ";
+    
+    cout << prompt1;
+    cin >> num1;
+    
+    cout << prompt2;
+    cin >> num2;
+
+    sum = num1 + num2;
+    cout << "The sum is: " << sum;
+
+
+    cout << endl;
+    return 0;
+}
